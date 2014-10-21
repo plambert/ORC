8a78b1058345b7b02c3bd10fac8a0e19ebe00ccb
Paul Lambert <plambert@plambert.net>
2014-10-21 14:07:23 -0700

    actually seems to work now!

---
+++ DSL/Script.pm
@@ -0,0 +1,22 @@
+#!/usr/bin/perl -w
+
+package DSL::Script;
+
+use strict;
+use warnings;
+use namespace::autoclean;
+use Moose;
+
+has 'statements' => (
+  is => 'rw',
+  required => 1
+);
+
+sub prettyprint {
+  my $self=shift;
+  return join('', map { $_->prettyprint } @{$self->statements});
+}
+
+__PACKAGE__->meta->make_immutable;
+
+1;
